26b01f0105863f1d5b247fb4245b318e01459f12
Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>
2025-09-02 04:08:25 +0000

    kernel: fixup! Move all SUS_ Flag checks from inode->i_state to inode->i_mapping->flags
    Signed-off-by: Sã Śâjjãd <103353616+Sa-Sajjad@users.noreply.github.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -2222,7 +2222,7 @@ struct dentry *__d_lookup_rcu(const struct dentry *parent,
 			continue;
 
                 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-			if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
+			if (dentry->d_inode && unlikely(dentry->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_non_root_user_app_proc())) {
 				continue;
 			}
                 #endif
@@ -2310,7 +2310,7 @@ struct dentry *__d_lookup(const struct dentry *parent, const struct qstr *name)
 			continue;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-		if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
+		if (dentry->d_inode && unlikely(dentry->d_inode->i_mapping->flags & BIT_SUS_PATH) && likely(susfs_is_current_non_root_user_app_proc())) {
 			continue;
 		}
 #endif
